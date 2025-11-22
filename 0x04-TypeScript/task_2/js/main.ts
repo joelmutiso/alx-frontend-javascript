4a6a8704f44354edb73d91ad385062cec4fc2b72
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-22 13:50:29 +0300

    Add Director and Teacher classes with createEmployee function

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,53 @@
+// 1. Create the DirectorInterface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// 2. Create the TeacherInterface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// 3. Create the Director class
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+// 4. Create the Teacher class
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// 5. Create the createEmployee function
+// Argument 'salary' can be a number OR a string
+export function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
+
+// --- Test Code (Optional) ---
+console.log(createEmployee(200));   // Teacher
+console.log(createEmployee(1000));  // Director
+console.log(createEmployee('$500'));// Director
