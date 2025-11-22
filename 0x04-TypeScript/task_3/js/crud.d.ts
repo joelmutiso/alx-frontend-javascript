3c9e46d546ee089393cfdf69d3766b9b39e2b4ec
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-22 16:07:40 +0300

    Added CRUD ambient types and main logic

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,7 +1,5 @@
-// Import the types from interface.ts
 import { RowID, RowElement } from './interface';
 
-// Declare the type signatures for the functions in crud.js
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(rowId: RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
+export declare function insertRow(row: RowElement): number;
+export declare function deleteRow(rowId: RowID): void;
+export declare function updateRow(rowId: RowID, row: RowElement): RowID;
